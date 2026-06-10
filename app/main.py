dd91926ebc940d9c56981006432cfe0d6f3215f0
Piramidiana <piramidiana1511@gmail.com>
2026-06-10 20:04:08 +0700

    Drafting auth and database logic

--- app/main.py
+++ app/main.py
@@ -13,3 +13,15 @@ app = FastAPI(
 @app.get("/")
 async def root():
     return {"message": "Welcome to KPT48 Ticketing System! Ready for the war."}
+
+    from fastapi import FastAPI
+from app.routers import auth # Import router baru
+
+app = FastAPI()
+
+# Daftarkan router
+app.include_router(auth.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Server KPT48 Ticketing Berjalan!"}
---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+
+# Schema ini dipakai saat user baru daftar (Register)
+class UserCreate(BaseModel):
+    email: EmailStr
+    username: str
+    full_name: str
+    nik: str
+    password: str
+
+# Schema ini dipakai saat menampilkan data user (tanpa password agar aman!)
+class UserResponse(BaseModel):
+    id_user: int
+    email: EmailStr
+    username: str
+    role: str
+
+    class Config:
+        from_attributes = True
