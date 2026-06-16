cd00251c23d21c878a9068fb8c815ed8a9968f8d
marceline-e <arrumantaekna@gmail.com>
2026-06-16 15:35:07 +0700

    ganti hash

--- app/database/seed.sql
+++ app/database/seed.sql
@@ -5,6 +5,6 @@ VALUES (
     'Keputih48 Admin', 
     '3578000000000001', 
     -- Catatan: password_hash ini adalah hasil enkripsi dari kata 'admin123' menggunakan bcrypt
-    '$2b$12$EixZaYVK1fsby1Z7KSt6O.V5a67mP4eS/85X2wO2r1lTETXm2V9aW', 
+    '$2b$12$Ml1XV/YabR5lUSjT5aTmZOj.Z.A/HFSfjCLcvzMY0d0lD7nnBs4sS', 
     'ADMIN'
 );
